f4dec3e39f24da51ca8be83f94e29bdcfec01503
Rafael Ferreira <ffael@live.com>
2020-11-08 22:44:55 -0500

    Adds Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,9 @@
+import Router from 'next/router'
+import * as gtag from '../libs/gtag'
+
+// Notice how we track pageview when route is changed
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
---
+++ pages/_document.js
@@ -0,0 +1,59 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from 'styled-components'
+
+export default class CustomDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    // Check if in production
+    const isProduction = process.env.NODE_ENV === "production";
+
+    return {
+      ...initialProps,
+      isProduction,
+    };
+  }
+
+  render() {
+    const { isProduction } = this.props;
+
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="shortcut icon" type="image/x-icon" />
+
+          {/* We only want to add the scripts if in production */}
+          {isProduction && (
+            <>
+              {/* Global Site Tag (gtag.js) - Google Analytics */}
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${process.env.GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
+            </>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
