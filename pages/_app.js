58c976a073a0816a23ebd7e491eb228c37b91a91
Rafael Ferreira <ffael@live.com>
2020-11-11 16:11:52 -0500

    Adds Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
-import Router from 'next/router'
-import * as gtag from '../libs/gtag'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../libs/gtag";
 
-// Notice how we track pageview when route is changed
-Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
+const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return <Component {...pageProps} />;
+};
 
-export default MyApp
+export default App;
